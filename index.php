499acc9c67c5d94fd5dcc8d5432b2b6508990193
DaviLeoMeda <davileo.meda@gmail.com>
2023-06-01 09:12:59 +0200

    third step

--- index.php
+++ index.php
@@ -15,10 +15,26 @@ class Movie{
 }
 
 $blockbuster = [
-    new Movie('Horror', 'The Deveoper assassin night', 'a crazy murder developer wants the city', '2 minutes');
-    new Movie('Fantastic', 'The Deveoper knights', 'a gay brave knight developer try to save his houseband', '1500 hours');
-    new Movie('Comedy', 'The Deveoper workout', 'a fat developer wants to be a bodybuilder', '120 minutes');
+    new Movie('Horror', 'The Deveoper assassin night', 'a crazy murder developer wants the city', '2 minutes'),
+    new Movie('Fantastic', 'The Deveoper knights', 'a gay brave knight developer try to save his houseband', '1500 hours'),
+    new Movie('Comedy', 'The Deveoper workout', 'a fat developer wants to be a bodybuilder', '120 minutes'),
 ];
 
-var_dump($blockbuster);
+// var_dump($blockbuster);
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlockBuster Afterlife</title>
+</head>
+<body>
+
+
+    
+</body>
+</html>
 ?>
